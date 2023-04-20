6e08b079cc48af4774c1da937fda62367d9db493
Husni Mubarak <husnimubarak859@gmail.com>
2023-04-20 20:48:58 +0700

    API endpoint work_experience

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userRouter = require('./user.js');
-const achievementRouter = require('./achievement.js');
+const userRouter = require("./user.js");
+const workExperienceRouter = require("./workExperience.js");
+const achievementRouter = require("./achievement.js");
 
 router.use(userRouter);
+router.use(workExperienceRouter);
 router.use(achievementRouter);
 
 module.exports = router;
