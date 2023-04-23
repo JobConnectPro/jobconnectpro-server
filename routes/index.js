befe9737c07279df5d32c4dbc6660e660ece9554
Ripa <105051709+ripasalam@users.noreply.github.com>
2023-04-23 14:24:10 +0700

    add organization api (#9)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userRouter = require('./user.js');
-const achievementRouter = require('./achievement.js');
+const userRouter = require("./user.js");
+const achievementRouter = require("./achievement.js");
+const organizationRouter = require("./organization.js");
 
 router.use(userRouter);
 router.use(achievementRouter);
+router.use(organizationRouter);
 
 module.exports = router;
--- routes/organization.js
+++ routes/organization.js
@@ -1,5 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const organizationController = require('../controllers/organizationController.js');
+const authentication = require("../middlewares/authentication.js");
+const OrganizationController = require("../controllers/organizationController.js");
 
+router.get(
+  "/organization",
+  authentication,
+  OrganizationController.findOrganization
+);
+router.post(
+  "/organization",
+  authentication,
+  OrganizationController.createOrganization
+);
+router.put("/organization/:id", authentication, OrganizationController.update);
+router.delete(
+  "/organization/:id",
+  authentication,
+  OrganizationController.destroy
+);
 module.exports = router;
