2e93fa62293e56e7f9a1cb7d232084cb5431fbc7
Husni-Mubarak <123556664+Husni-Mubarak@users.noreply.github.com>
2023-04-23 14:27:26 +0700

    API endpoint work_experience (#10)
    Co-authored-by: Aruf Rachman Hakim <53162307+arufhakim@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const userRouter = require("./user.js");
+const workExperienceRouter = require("./workExperience.js");
 const achievementRouter = require("./achievement.js");
 const organizationRouter = require("./organization.js");
 
 router.use(userRouter);
+router.use(workExperienceRouter);
 router.use(achievementRouter);
 router.use(organizationRouter);
 
