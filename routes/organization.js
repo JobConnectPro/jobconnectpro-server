e49875bb5da39448c9985f84120178d4f2de08a5
Aruf Rachman Hakim <arufhakim@gmail.com>
2023-04-24 12:50:27 +0700

    add authorization, update user and organization

--- routes/organization.js
+++ routes/organization.js
@@ -1,22 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const authentication = require("../middlewares/authentication.js");
-const OrganizationController = require("../controllers/organizationController.js");
+const authentication = require('../middlewares/authentication.js');
+const authorization = require('../middlewares/authorization.js');
+const OrganizationController = require('../controllers/organizationController.js');
 
-router.get(
-  "/organization",
-  authentication,
-  OrganizationController.findOrganization
-);
-router.post(
-  "/organization",
-  authentication,
-  OrganizationController.createOrganization
-);
-router.put("/organization/:id", authentication, OrganizationController.update);
-router.delete(
-  "/organization/:id",
-  authentication,
-  OrganizationController.destroy
-);
+router.get('/organizations', authentication, authorization(['Seeker']), OrganizationController.findOrganizations);
+router.get('/organizations/:organizationId', authentication, authorization(['Seeker']), OrganizationController.findOrganization);
+router.post('/organizations', authentication, authorization(['Seeker']), OrganizationController.createOrganization);
+router.put('/organizations/:organizationId', authentication, authorization(['Seeker']), OrganizationController.updateOrganization);
+router.delete('/organizations/:organizationId', authentication, authorization(['Seeker']), OrganizationController.destroyOrganization);
+
 module.exports = router;
