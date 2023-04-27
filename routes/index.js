af150da906cd7974851d0dc90a2e1be022ca58a5
Aruf Rachman Hakim <53162307+arufhakim@users.noreply.github.com>
2023-04-27 19:21:30 +0700

    Feature (#16)
    * API CRUD Company and Sector
    
    * API CRUD Company  and Sector
    
    * edit errorhandler
    
    ---------
    
    Co-authored-by: ramadhan ramadhan <danramadhan0@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const userRouter = require("./user.js");
+
+const userRouter = require('./user.js');
+const achievementRouter = require('./achievement.js');
+const companyRouter = require('./company.js');
+const sectorRouter = require('./sector.js');
 const workExperienceRouter = require("./workExperience.js");
-const achievementRouter = require("./achievement.js");
 const organizationRouter = require("./organization.js");
 const skillRouter = require('./skill.js');
 
 router.use(userRouter);
 router.use(workExperienceRouter);
 router.use(achievementRouter);
+router.use(companyRouter);
+router.use(sectorRouter);
 router.use(organizationRouter);
 router.use(skillRouter);
 
--- routes/sector.js
+++ routes/sector.js
@@ -1,5 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const sectorController = require('../controllers/sectorController.js');
+const authentication = require('../middlewares/authentication.js');
+
+router.get(
+    '/sector',
+    authentication,
+    sectorController.getAllSector
+)
+router.get(
+    '/sector/:id',
+    authentication,
+    sectorController.getSectorById
+)
+router.post(
+    '/sector',
+    authentication,
+    sectorController.createSector
+)
+router.put(
+    '/sector/:id',
+    authentication,
+    sectorController.updateSectorById
+)
+router.delete(
+    '/sector/:id',
+    authentication,
+    sectorController.deleteSectorById
+)
 
 module.exports = router;
