cc262c28a0656c313acf0fd19c437d7a7204787e
Aruf Rachman Hakim <arufhakim@gmail.com>
2023-04-27 19:25:16 +0700

    add project features

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userRouter = require("./user.js");
-const workExperienceRouter = require("./workExperience.js");
-const achievementRouter = require("./achievement.js");
-const organizationRouter = require("./organization.js");
+const userRouter = require('./user.js');
+const workExperienceRouter = require('./workExperience.js');
+const achievementRouter = require('./achievement.js');
+const organizationRouter = require('./organization.js');
 const skillRouter = require('./skill.js');
+const projectRouter = require('./project.js');
 
 router.use(userRouter);
 router.use(workExperienceRouter);
 router.use(achievementRouter);
 router.use(organizationRouter);
 router.use(skillRouter);
+router.use(projectRouter);
 
 module.exports = router;
--- routes/project.js
+++ routes/project.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const projectController = require('../controllers/projectController.js');
+const authentication = require('../middlewares/authentication.js');
+const authorization = require('../middlewares/authorization.js');
+const ProjectController = require('../controllers/projectController.js');
+
+router.get('/projects', authentication, authorization(['Seeker']), ProjectController.findProjects);
+router.get('/projects/:projectId', authentication, authorization(['Seeker']), ProjectController.findProject);
+router.post('/projects', authentication, authorization(['Seeker']), ProjectController.createProject);
+router.put('/projects/:projectId', authentication, authorization(['Seeker']), ProjectController.updateProject);
+router.delete('/projects/:projectId', authentication, authorization(['Seeker']), ProjectController.destroyProject);
 
 module.exports = router;
