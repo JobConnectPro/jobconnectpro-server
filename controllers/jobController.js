2af17563a8eb9293447458b61459918b87701c1e
arthagp <55339834+arthagp@users.noreply.github.com>
2023-04-30 21:30:05 +0700

    Update job category (#25)
    * mengganti addCategories menjadi setJobCategories di karenakan pada relasinya menggunakan as jobCategories
    
    * Update bagian findJobs(menambahkan filter berdasarkan title,type,location, dan minimum_exp), CreateJob(mengubah nama method addCategory, menjadi addJobCategory), UpdateJob

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,9 +1,28 @@
+
 const { Job, Category, Company, Sector } = require('../models');
+const { Op } = require('sequelize');
 
 class JobController {
   static async findJobs(req, res, next) {
     try {
+      const { title, type, location, minimum_experience } = req.query;
+      const where = {};
+  
+      if (title) {
+        where.title = { [Op.iLike]: `%${title}%` };
+      }
+      if (type) {
+        where.type = { [Op.iLike]: `%${type}%` };
+      }
+      if (location) {
+        where.location = { [Op.iLike]: `%${location}%` };
+      }
+      if (minimum_experience) {
+        where.minimum_experience = { [Op.gte]: minimum_experience };
+      }
+  
       const data = await Job.findAll({
+        where,
         include: [
           {
             model: Company,
@@ -20,6 +39,7 @@ class JobController {
       next(error);
     }
   }
+  
 
   static async findJob(req, res, next) {
     try {
@@ -49,95 +69,106 @@ class JobController {
     }
   }
 
-  static async createJob(req, res) {
+  static async createJob(req, res, next) {
+  const { title, description, categories, requirement, job_level, minimum_salary, maximum_salary, type, location, starting_date, minimum_experience } = req.body;
+
+  const userId = req.userLogged.id;
+
+  try {
+    const company = await Company.findOne({
+      where: { user_id: userId },
+    });
+
+    if (!company) {
+      return res.status(404).json({ message: 'Company Not Found' });
+    }
+
+    const job = await Job.create({
+      user_id: userId,
+      company_id: company.id,
+      title,
+      description,
+      requirement,
+      job_level,
+      minimum_salary,
+      maximum_salary,
+      type,
+      location,
+      starting_date,
+      minimum_experience,
+    });
+
+    if (categories && categories.length > 0) {
+      const categoriesInstance = await Category.findAll({
+        where: { category: categories },
+      });
+
+      if (!categoriesInstance || categoriesInstance.length === 0) {
+        throw { name: 'ErrorNotFound' };
+      }
+
+      await job.addJobCategories(categoriesInstance);
+    }
+
+    res.status(201).json({
+      message: 'Job Created',
+      fullField: {
+        data: job,
+        company: {
+          companyName: company.company_name,
+        },
+        category: {
+          categoryName : categories
+        }
+      },
+    });
+  } catch (error) {
+    next(error)
+  }
+}
+
+static async updateJob(req, res, next) {
+  try {
+    const { jobId } = req.params;
     const { title, description, categories, requirement, job_level, minimum_salary, maximum_salary, type, location, starting_date, minimum_experience } = req.body;
 
-    const userId = req.userLogged.id;
-
-    try {
-      const company = await Company.findOne({
-        where: { user_id: userId },
-      });
-
-      if (!company) {
-        return res.status(404).json({ message: 'Company Not Found' });
-      }
-
-      const job = await Job.create({
-        user_id: userId,
-        company_id: company.id,
-        title,
-        description,
-        requirement,
-        job_level,
-        minimum_salary,
-        maximum_salary,
-        type,
-        location,
-        starting_date,
-        minimum_experience,
-      });
-
-      if (categories && categories.length > 0) {
-        const categoriesInstance = await Category.findAll({
-          where: { category: categories },
-        });
-        await job.addCategories(categoriesInstance);
-      }
-
-      res.status(201).json({
-        message: 'job created',
-        fullField: {
-          data: job,
-          company: {
-            companyName: company.company_name,
-          },
-        },
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Internal Server Error');
+    const job = await Job.findByPk(jobId);
+    
+    if (!job) {
+      return res.status(404).send('Job not found');
     }
-  }
-
-  static async updateJob(req, res) {
-    const { jobId } = req.params;
-    const { company_id, title, description, categories, requirement, job_level, minimum_salary, maximum_salary, type, location, starting_date, minimum_experience } = req.body;
-    try {
-      const job = await Job.findByPk(jobId);
-      if (job) {
-        job.company_id = company_id;
-        job.title = title;
-        job.description = description;
-        job.requirement = requirement;
-        job.job_level = job_level;
-        job.minimum_salary = minimum_salary;
-        job.maximum_salary = maximum_salary;
-        job.type = type;
-        job.location = location;
-        job.starting_date = starting_date;
-        job.minimum_experience = minimum_experience;
-        await job.save();
-        if (categories && categories.length > 0) {
-          const categoriesInstance = await Category.findAll({
-            where: { category: categories },
-          });
-          await job.setCategories(categoriesInstance);
-        } else {
-          await job.setCategories([]);
-        }
-        res.status(200).json({
-          message: 'Updated Succesfully',
-          updatedData: job,
-        });
-      } else {
-        res.status(404).send('Job not found');
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).send('Internal Server Error');
+    
+    const updatedJob = await job.update({
+      title,
+      description,
+      requirement,
+      job_level,
+      minimum_salary,
+      maximum_salary,
+      type,
+      location,
+      starting_date,
+      minimum_experience,
+    });
+    
+    if (categories && categories.length > 0) {
+      const categoriesInstance = await Category.findAll({
+        where: { category: categories },
+      });
+      
+      await updatedJob.setJobCategories(categoriesInstance);
+    } else {
+      await updatedJob.setJobCategories([]);
     }
+    
+    res.status(200).json({
+      message: 'Updated Successfully',
+      updatedData: updatedJob,
+    });
+  } catch (error) {
+    next()
   }
+}
 
   static async destroyJob(req, res) {
     const { jobId } = req.params;
