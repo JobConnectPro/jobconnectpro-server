d6ff5a89234ffa5f697e180f97d1da214d8b0397
arthagp <55339834+arthagp@users.noreply.github.com>
2023-05-06 12:17:18 +0700

    update jobController pada bagian createJob, dan UpdateJob, perubahan meliputi pagination, unmanaged transaction, penambahan req body status dan company_id (#33)

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,4 @@
-const { Job, Category, Company, Sector } = require('../models');
+const { Job, Category, Company, Sector, sequelize } = require('../models');
 const { Op } = require('sequelize');
 
 class JobController {
@@ -20,11 +20,11 @@ class JobController {
         where.minimum_experience = { [Op.lte]: minimum_experience };
       }
 
-      const limit = req.query.limit || 10;
-      const page = req.query.page || 1;
+      const limit = +req.query.limit || 10;
+      const page = +req.query.page || 1;
       const offset = (page - 1) * limit;
 
-      const data = await Job.findAll({
+      const {count, rows} = await Job.findAndCountAll({
         where,
         limit,
         offset,
@@ -40,7 +40,12 @@ class JobController {
           },
         ],
       });
-      res.status(200).json(data);
+      res.status(200).json({
+        totalItems : count,
+        data : rows,
+        currentPage : page,
+        totalPages : Math.ceil(count/limit)
+      });
     } catch (error) {
       next(error);
     }
@@ -75,18 +80,33 @@ class JobController {
   }
 
   static async createJob(req, res, next) {
+    const t = await sequelize.transaction();
     try {
       const userId = req.userLogged.id;
-      const { title, description, categories, requirement, job_level, minimum_salary, maximum_salary, type, location, starting_date, minimum_experience } = req.body;
-
+      const { title, company_id, description, categories, requirement, job_level, minimum_salary, maximum_salary, type, location, starting_date, minimum_experience , status } = req.body;
+  
       const company = await Company.findOne({
-        where: { user_id: userId },
+        where: { id: company_id}
+        
       });
-
       if (!company) {
         return res.status(404).json({ message: 'Company not found!' });
       }
-
+  
+      if (!categories || categories.length === 0) {
+        return res.status(400).json({message: 'Categories must be provided!' });
+      }
+  
+      const categoriesInstance = await Category.findAll({
+        where: { category: categories },
+      }, { transaction: t });
+  
+      if (!categoriesInstance || categoriesInstance.length === 0) {
+        return res.status(400).json({
+          message: 'Category Not Found'
+        })
+      }
+  
       const job = await Job.create({
         user_id: userId,
         company_id: company.id,
@@ -100,20 +120,13 @@ class JobController {
         location,
         starting_date,
         minimum_experience,
-      });
-
-      if (categories && categories.length > 0) {
-        const categoriesInstance = await Category.findAll({
-          where: { category: categories },
-        });
-
-        if (!categoriesInstance || categoriesInstance.length === 0) {
-          throw { name: 'ErrorNotFound' };
-        }
-
-        await job.addJobCategories(categoriesInstance);
-      }
-
+        status
+      }, { transaction: t });
+  
+      await job.addJobCategories(categoriesInstance, { transaction: t });
+  
+      await t.commit();
+  
       res.status(201).json({
         message: 'Successfully create job!',
         fullField: {
@@ -127,22 +140,36 @@ class JobController {
         },
       });
     } catch (error) {
+      await t.rollback();
       next(error);
     }
   }
+  
+
 
   static async updateJob(req, res, next) {
+    const t = await sequelize.transaction();
     try {
       const { id } = req.userLogged;
       const { jobId } = req.params;
-      const { title, description, categories, requirement, job_level, minimum_salary, maximum_salary, type, location, starting_date, minimum_experience } = req.body;
-
+      const { title, company_id, description, categories, requirement, job_level, minimum_salary, maximum_salary, type, location, starting_date, minimum_experience, status } = req.body;
+  
       const job = await Job.findOne({ where: { id: jobId, user_id: id } });
-
+  
       if (!job) {
         return res.status(404).json({ message: 'Job not found!' });
       }
-
+  
+      if (company_id) {
+        const company = await Company.findOne({ where: { id: company_id } });
+  
+        if (!company) {
+          return res.status(404).json({ message: 'Company not found!' });
+        }
+  
+        await job.update({ company_id: company.id }, { transaction: t });
+      }
+  
       const updatedJob = await job.update({
         title,
         description,
@@ -154,26 +181,38 @@ class JobController {
         location,
         starting_date,
         minimum_experience,
-      });
-
-      if (categories && categories.length > 0) {
-        const categoriesInstance = await Category.findAll({
-          where: { category: categories },
-        });
-
-        await updatedJob.setJobCategories(categoriesInstance);
-      } else {
-        await updatedJob.setJobCategories([]);
+        status
+      }, { transaction: t });
+  
+      if (!categories || categories.length === 0) {
+        return res.status(400).json({message: 'Categories must be provided!' });
       }
-
+  
+      const categoriesInstance = await Category.findAll({
+        where: { category: categories },
+      }, { transaction: t });
+  
+      if (!categoriesInstance || categoriesInstance.length === 0) {
+        throw { name: 'ErrorNotFound' };
+      }
+  
+      await updatedJob.setJobCategories(categoriesInstance, { transaction: t });
+  
+      await t.commit();
+  
       res.status(200).json({
         message: 'Successfully update job!',
         updatedData: updatedJob,
       });
     } catch (error) {
+      await t.rollback();
       next(error);
     }
   }
+  
+  
+  
+  
 
   static async destroyJob(req, res, next) {
     try {
@@ -195,3 +234,6 @@ class JobController {
 }
 
 module.exports = JobController;
+
+
+
