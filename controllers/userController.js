61fc66bfe1f4b97019d347f6e9496e5b2ebde0c0
Ripa <105051709+ripasalam@users.noreply.github.com>
2023-05-15 18:21:50 +0700

    edit userController (#49)
    * edit userController
    
    * cadd change password after login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,27 @@
 require('dotenv').config();
-const { User, Company, WorkExperience, Skill, Education, Project, Organization, Achievement, Attainment, Application, Bookmark, Job, UserSkill, Sector } = require('../models');
+const {
+  User,
+  Company,
+  WorkExperience,
+  Skill,
+  Education,
+  Project,
+  Organization,
+  Achievement,
+  Attainment,
+  Application,
+  Bookmark,
+  Job,
+  UserSkill,
+  Sector,
+} = require('../models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const sendMail = require('../email');
 const sendMailForgotPassword = require('../email-forgot-password');
 const { where } = require('sequelize');
 
-class UserController {
+class userController {
   // get all user
   static async findUsers(req, res, next) {
     try {
@@ -14,7 +29,11 @@ class UserController {
       const page = +req.query.page || 1;
       const offset = (page - 1) * limit;
 
-      const { count, rows } = await User.findAndCountAll({ limit, offset, order: [['role', 'ASC']] });
+      const { count, rows } = await User.findAndCountAll({
+        limit,
+        offset,
+        order: [['role', 'ASC']],
+      });
       res.status(200).json({
         totalItems: count,
         data: rows,
@@ -79,7 +98,15 @@ class UserController {
   static async updateUser(req, res, next) {
     try {
       const { id } = req.userLogged;
-      const { name, birthday, gender, phone, address, summary, salary_expectation } = req.body;
+      const {
+        name,
+        birthday,
+        gender,
+        phone,
+        address,
+        summary,
+        salary_expectation,
+      } = req.body;
 
       const findUser = await User.findOne({ where: { id } });
 
@@ -109,14 +136,26 @@ class UserController {
   static async updatePassword(req, res, next) {
     try {
       const { id } = req.userLogged;
-      const { password } = req.body;
+      const { currentPassword, newPassword } = req.body;
+      console.log(id, currentPassword, newPassword);
 
       const findUser = await User.findOne({ where: { id } });
 
       if (findUser) {
-        const hashPassword = await bcrypt.hash(password, 10);
-        const data = await User.update({ password: hashPassword }, { where: { id } });
-        res.status(200).json({ message: 'Successfully update password!' });
+        const comparePassword = await bcrypt.compare(
+          currentPassword,
+          findUser.password
+        );
+        if (comparePassword) {
+          const hashPassword = await bcrypt.hash(newPassword, 10);
+          const data = await User.update(
+            { password: hashPassword },
+            { where: { id } }
+          );
+          res.status(200).json({ message: 'Successfully update password!' });
+        } else {
+          throw { name: 'WrongPassword' };
+        }
       } else {
         throw { name: 'ErrorNotFound' };
       }
@@ -189,7 +228,9 @@ class UserController {
         where: {
           id,
         },
-        include: [{ model: Job, as: 'UserApplication', include: [{ model: Company }] }],
+        include: [
+          { model: Job, as: 'UserApplication', include: [{ model: Company }] },
+        ],
       });
 
       if (data) {
@@ -222,7 +263,9 @@ class UserController {
           user_id: id,
           job_id,
         });
-        res.status(201).json({ ...data.dataValues, message: 'Successfully apply job!' });
+        res
+          .status(201)
+          .json({ ...data.dataValues, message: 'Successfully apply job!' });
       }
     } catch (error) {
       next(error);
@@ -266,7 +309,9 @@ class UserController {
         where: {
           id,
         },
-        include: [{ model: Job, as: 'UserBookmark', include: [{ model: Company }] }],
+        include: [
+          { model: Job, as: 'UserBookmark', include: [{ model: Company }] },
+        ],
       });
 
       if (data) {
@@ -299,7 +344,9 @@ class UserController {
           user_id: id,
           job_id,
         });
-        res.status(201).json({ ...data.dataValues, message: 'Successfully bookmark job!' });
+        res
+          .status(201)
+          .json({ ...data.dataValues, message: 'Successfully bookmark job!' });
       }
     } catch (error) {
       next(error);
@@ -605,7 +652,9 @@ class UserController {
           skill_id,
           level,
         });
-        res.status(201).json({ ...data.dataValues, message: 'Successfully add skill!' });
+        res
+          .status(201)
+          .json({ ...data.dataValues, message: 'Successfully add skill!' });
       }
     } catch (error) {
       next(error);
@@ -644,7 +693,8 @@ class UserController {
   // user register
   static async register(req, res, next) {
     try {
-      const { name, email, password, role, birthday, gender, phone, address } = req.body;
+      const { name, email, password, role, birthday, gender, phone, address } =
+        req.body;
 
       const uniqueEmail = await User.findOne({
         where: {
@@ -663,7 +713,9 @@ class UserController {
           phone,
           address,
         });
-        res.status(201).json({ ...data.dataValues, message: 'Successfully register!' });
+        res
+          .status(201)
+          .json({ ...data.dataValues, message: 'Successfully register!' });
       } else {
         throw { name: 'UserExist' };
       }
@@ -682,7 +734,10 @@ class UserController {
       });
 
       if (findUser) {
-        const comparePassword = await bcrypt.compare(password, findUser.password);
+        const comparePassword = await bcrypt.compare(
+          password,
+          findUser.password
+        );
         if (comparePassword) {
           const token = jwt.sign(
             {
@@ -721,7 +776,11 @@ class UserController {
       const { id } = findUser;
 
       if (findUser) {
-        const resetLink = jwt.sign({ email: findUser.email }, process.env.JWT_SECRET, { expiresIn: '12h' });
+        const resetLink = jwt.sign(
+          { email: findUser.email },
+          process.env.JWT_SECRET,
+          { expiresIn: '12h' }
+        );
         const emailData = {
           id: findUser.id,
           name: findUser.name,
@@ -744,7 +803,7 @@ class UserController {
       next(err);
     }
   }
-  
+
   static async resetPassword(req, res, next) {
     const { password } = req.body;
     const resetLink = req.params.token;
@@ -775,4 +834,4 @@ class UserController {
   }
 }
 
-module.exports = UserController;
+module.exports = userController;
